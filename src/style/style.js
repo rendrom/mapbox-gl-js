45033c62bc4b50c0c0282091f0f7954ec202d863
ryanhamley <ryan.hamley@mapbox.com>
2018-11-20 14:08:48 -0800

    Make textureQueue a private property

--- src/style/style.js
+++ src/style/style.js
@@ -118,7 +118,7 @@ class Style extends Evented {
     _removedLayers: {[string]: StyleLayer};
     _updatedPaintProps: {[layer: string]: true};
     _layerOrderChanged: boolean;
-    textureQueue: Array<Object>;
+    _textureQueue: Array<Object>;
 
     crossTileSymbolIndex: CrossTileSymbolIndex;
     pauseablePlacement: PauseablePlacement;
@@ -145,7 +145,7 @@ class Style extends Evented {
         this.sourceCaches = {};
         this.zoomHistory = new ZoomHistory();
         this._loaded = false;
-        this.textureQueue = [];
+        this._textureQueue = [];
 
         this._resetUpdates();
 
